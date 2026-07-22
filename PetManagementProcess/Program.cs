0916879afc2234ee0e1f8354de977063dbc3b5e1
Lufuno-software <nenje@LAPTOP-3VR2AVB8>
2026-07-22 18:45:39 +0200

    Add project files.

---
+++ PetManagementProcess/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetManagementProcess
+{ //Class
+    class Pet
+    {
+        //Attributes
+        public string name;
+        public string breed;
+        public int age;
+
+        //Method
+        public void Eat()
+        {
+            Console.WriteLine("The dog is eating its food.");
+        }
+        public void Sleep()
+        {
+            Console.WriteLine("The dog is sleeping.");
+        }
+        public void Play()
+        {
+            Console.WriteLine("The dog like to play catch.");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        { //Object
+            Pet mypet = new Pet();
+            mypet.name = "Rex";
+            mypet.breed = "Jack Russell Terrier";
+            mypet.age = 10;
+            Console.WriteLine("Name: " + mypet.name);
+            Console.WriteLine("Breed: " + mypet.breed);
+            Console.WriteLine("Age: " + mypet.age);
+
+            mypet.Eat();
+            mypet.Sleep();
+            mypet.Play();
+
+        }
+    }
+}
